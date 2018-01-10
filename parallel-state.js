9f9882e8b722bc6d3359f020a6043350996eb2e2
Bruno Konrad <bruno.skon@gmail.com>
2018-01-10 17:00:09 +0100

    Creates parallel state constructor
    - It follows description from README.md

---
+++ parallel-state.js
@@ -0,0 +1,41 @@
+var Nanobus = require('nanobus')
+var assert = require('assert')
+
+module.exports = Parallelstate
+
+function Parallelstate (transitions) {
+  if (!(this instanceof Parallelstate)) return new Parallelstate(transitions)
+  assert.equal(typeof transitions, 'object', 'nanostate: transitions should be type object')
+
+  this.scopes = Object.keys(transitions)
+  this.transitions = transitions
+
+  this.scopes.forEach(scope => {
+    assert.ok(transitions[scope] instanceof Nanobus, `parallel expects "${scope}" to have nanostate as transition`)
+  })
+
+  Object.defineProperty(this, 'state', {
+    get: function () {
+      return this.scopes.reduce((state, scope) => {
+        state[scope] = this.transitions[scope].state
+        return state
+      }, {})
+    }
+  })
+
+  Nanobus.call(this)
+}
+
+Parallelstate.prototype = Object.create(Nanobus.prototype)
+
+Parallelstate.prototype.emit = function (eventName) {
+  var hasCollon = eventName.indexOf(':') >= 0
+  assert.ok(hasCollon, `nanostate.emit: invalid transition ${this.state} -> ${eventName}. For parallel nanostate eventName must have a collon ":"`)
+
+  var [scope, event] = eventName.split(':')
+  assert.ok(scope, `nanostate.emit: invalid scope ${scope} for parallel emitting`)
+
+  this.transitions[scope].emit(event)
+
+  Nanobus.prototype.emit.call(this, eventName)
+}
--- test.js
+++ test.js
@@ -36,3 +36,59 @@ function move (assert, machine, states) {
     assert.equal(machine.state, expected, `from ${initial} to ${expected}`)
   })
 }
+
+var createParallelTransitions = () => ({
+  bold: nanostate('off', {
+    on: { 'toggle': 'off' },
+    off: { 'toggle': 'on' }
+  }),
+  underline: nanostate('off', {
+    on: { 'toggle': 'off' },
+    off: { 'toggle': 'on' }
+  }),
+  italics: nanostate('off', {
+    on: { 'toggle': 'off' },
+    off: { 'toggle': 'on' }
+  }),
+  list: nanostate('none', {
+    none: { bullets: 'bullets', numbers: 'numbers' },
+    bullets: { none: 'none', numbers: 'numbers' },
+    numbers: { bullets: 'bullets', none: 'none' }
+  })
+})
+
+tape('create parallel state', (assert) => {
+  var machine = nanostate.parallel(createParallelTransitions())
+
+  machine.emit('bold:toggle')
+  assert.deepEqual(machine.state, {
+    bold: 'on', underline: 'off', italics: 'off', list: 'none'
+  })
+
+  assert.end()
+})
+
+tape('change states in parallel machine', (assert) => {
+  var machine = nanostate.parallel(createParallelTransitions())
+
+  machine.emit('underline:toggle')
+  machine.emit('list:numbers')
+  assert.deepEqual(machine.state, {
+    bold: 'off', underline: 'on', italics: 'off', list: 'numbers'
+  })
+
+  machine.emit('bold:toggle')
+  machine.emit('underline:toggle')
+  machine.emit('italics:toggle')
+  machine.emit('list:bullets')
+  assert.deepEqual(machine.state, {
+    bold: 'on', underline: 'off', italics: 'on', list: 'bullets'
+  })
+
+  machine.emit('list:none')
+  assert.deepEqual(machine.state, {
+    bold: 'on', underline: 'off', italics: 'on', list: 'none'
+  })
+
+  assert.end()
+})
